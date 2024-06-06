f516a6a9e6e502aa2c6c592c30cc0e43da171c2a
shahin8433 <mdshahinalamsss6027@gmail.com>
2024-06-06 08:22:42 +0600

    add call

--- anisul.js
+++ anisul.js
@@ -592,6 +592,8 @@
 //                     console.log(`${x} : ${student[x]}`)
 // }
 
+
+
 //////////////////////////////////////////////////////////////////////////////
 ///////////  class-70: forEach vs for ///////////////////////////
 // var numbers =[5,7,8,9];
@@ -968,4 +970,42 @@
 // }
 // callAllTask()
 
+// console.log("end");
+
+
+// console.log("wellcome");
+// const taskOne=()=>{
+//                     return new Promise((resolve,reject)=>{
+//                                         resolve("task 1 completed")
+//                     })
+// }
+// const taskTwo= ()=>{
+//                     return new Promise((resolve, reject)=>{
+//                                         setTimeout(()=>{
+//                                                             resolve("task 2 completed")
+//                                         }, 3000)
+//                     })
+// }
+// const taskThree=()=>{
+//                       return new Promise((resolve, reject)=>{
+//                         resolve("task 3 is completed")
+//                     //     reject("task 3 is not completed")
+//                       })
+//                     }
+//   const taskFour=()=>{
+//                       return new Promise((resolve, reject)=>{
+//                         resolve("task 4 is completed")
+//                       })
+//                     }
+// async function callAllTask(){
+//                     const t1 = await taskOne();
+//                     console.log(t1);
+//                     const t2 = await taskTwo();
+//                     console.log(t2);
+//                     const t3 = await taskThree();
+//                     console.log(t3);
+//                     const t4 = await taskFour();
+//                     console.log(t4);
+// }
+// callAllTask()
 // console.log("end");
